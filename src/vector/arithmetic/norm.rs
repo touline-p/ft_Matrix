b7a7e7a16a46e80cee78b6151ce5089e4cbf2bed
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-08-28 14:51:40 +0200

    feat and test(norm)

--- src/vector/arithmetic/norm.rs
+++ src/vector/arithmetic/norm.rs
@@ -1,14 +1,25 @@
 use super::{Vector, Field};
+use crate::vector::traits::Abs;
 
 impl<K: Field, const SIZE: usize> Vector<K, SIZE> {
-    pub fn norm_1(&self) -> K {
-        self.iter().fold(K::default(), |acc, x| acc + *x)
+    pub fn norm_1(&self) -> K
+        where K: PartialOrd + Abs {
+        self.iter().fold(K::default(), |acc, x| acc + x.abs())
     }
     pub fn norm_inf(&self) -> K
-        where K:Ord {
-        self.iter().map(|x| {if *x < K::default()
-            {*x - *x - *x} else {*x}
-        }).max().unwrap()
+        where K: PartialOrd + Abs {
+        self.iter()
+            .map(|x| {x.abs()})
+            .fold(
+                K::default(),
+                |acc, x| {
+                    if x > acc {
+                        x
+                    } else {
+                        acc
+                    }
+                }
+            )
     }
     pub fn norm(&self) -> f32
         where K: Into<f32>
@@ -17,3 +28,29 @@ impl<K: Field, const SIZE: usize> Vector<K, SIZE> {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use crate::vector::Vector;
+    #[test]
+    fn vector_null() {
+        let u = Vector::from([0., 0., 0.]);
+        let result = 0.;
+        assert_eq!(u.norm_1(), result);
+        assert_eq!(u.norm(), result);
+        assert_eq!(u.norm_inf(), result);
+    }
+    #[test]
+    fn positiv_vec() {
+        let u = Vector::from([1., 2., 3.]);
+        assert_eq!(u.norm_1(), 6.0);
+        assert_eq!(u.norm(), 3.7416575);
+        assert_eq!(u.norm_inf(), 3.0);
+    }
+    #[test]
+    fn negativ_vec() {
+        let u = Vector::from([-1., -2.]);
+        assert_eq!(u.norm_1(), 3.0);
+        assert_eq!(u.norm(), 2.236067977);
+        assert_eq!(u.norm_inf(), 2.0);
+    }
+}
